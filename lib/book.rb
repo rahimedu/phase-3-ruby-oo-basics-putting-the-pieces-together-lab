c27d28c5a857b950b6813cdd39a31a2d9c04265f
rahimedu <edwin.duche82@gmail.com>
2022-08-19 17:21:07 -0400

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
 
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def initialize(book)
+        @title=book
+        @author=""
+        @page_count = 0
+        @genre = ""
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
